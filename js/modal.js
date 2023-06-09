6f4c9404af871c28dde84677bba355b2e8431aef
VeroVH <vero.vh@gmail.com>
2023-06-09 16:35:59 -0500

    limpieza master branch del modal

--- js/modal.js
+++ js/modal.js
@@ -1,37 +1,14 @@
 (() => {
-    const refs = {
-      openModalBtn: document.querySelector(".banner-button"),
-      closeModalBtn: document.querySelector(".close-modal"),
-      modal: document.querySelector(".modal"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector(".banner-button"),
-//     closeModalBtn: document.querySelector(".close-modal"),
-//     modal: document.querySelector(".modal"),
-//   };
-
-//   refs.openModalBtn.addEventListener("click", openModal);
-//   refs.closeModalBtn.addEventListener("click", closeModal);
-
-//   function openModal() {
-//     refs.modal.classList.add("modal-open");
-//     refs.modal.classList.remove("modal-closed");
-//   }
-  
-//   function closeModal() {
-//       refs.modal.classList.add("modal-closed");
-//       refs.modal.classList.remove("modal-open");
-//    }
-// })();
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
